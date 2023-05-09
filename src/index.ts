9f03a635737134d0487cbee3e30ec052b3dfa174
ligdy <dy179912099@163.com>
2023-05-09 14:53:59 +0800

    feat: index change

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { default as useProportion } from "./useProportion";
 export { default as useScreenTranslate } from "./useScreenTranslate";
 export { default as useWindowSize } from "./useWindowSize";
+export { default as useFetch } from "./useFetch";
